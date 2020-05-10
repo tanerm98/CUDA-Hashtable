592a898bec56fb18fb23c5cd752d28a7df1c758b
Ubuntu 19.10 <taner.mustafa.98@gmail.com>
2020-05-10 15:21:17 -0700

    try check

--- to_queue.sh
+++ to_queue.sh
@@ -2,3 +2,64 @@
 
 qsub -q hp-sl.q -b y -wd ~/CUDA-Hashtable ./module_load.sh
 qstat
+sleep 30m
+
+qsub -q hp-sl.q -b y -wd ~/CUDA-Hashtable ./module_load.sh
+qstat
+sleep 30m
+
+qsub -q hp-sl.q -b y -wd ~/CUDA-Hashtable ./module_load.sh
+qstat
+sleep 30m
+
+qsub -q hp-sl.q -b y -wd ~/CUDA-Hashtable ./module_load.sh
+qstat
+sleep 30m
+
+qsub -q hp-sl.q -b y -wd ~/CUDA-Hashtable ./module_load.sh
+qstat
+sleep 30m
+
+qsub -q hp-sl.q -b y -wd ~/CUDA-Hashtable ./module_load.sh
+qstat
+sleep 30m
+
+qsub -q hp-sl.q -b y -wd ~/CUDA-Hashtable ./module_load.sh
+qstat
+sleep 30m
+
+qsub -q hp-sl.q -b y -wd ~/CUDA-Hashtable ./module_load.sh
+qstat
+sleep 30m
+
+qsub -q hp-sl.q -b y -wd ~/CUDA-Hashtable ./module_load.sh
+qstat
+sleep 30m
+
+qsub -q hp-sl.q -b y -wd ~/CUDA-Hashtable ./module_load.sh
+qstat
+sleep 30m
+
+qsub -q hp-sl.q -b y -wd ~/CUDA-Hashtable ./module_load.sh
+qstat
+sleep 30m
+
+qsub -q hp-sl.q -b y -wd ~/CUDA-Hashtable ./module_load.sh
+qstat
+sleep 30m
+
+qsub -q hp-sl.q -b y -wd ~/CUDA-Hashtable ./module_load.sh
+qstat
+sleep 30m
+
+qsub -q hp-sl.q -b y -wd ~/CUDA-Hashtable ./module_load.sh
+qstat
+sleep 30m
+
+qsub -q hp-sl.q -b y -wd ~/CUDA-Hashtable ./module_load.sh
+qstat
+sleep 30m
+
+qsub -q hp-sl.q -b y -wd ~/CUDA-Hashtable ./module_load.sh
+qstat
+sleep 30m
