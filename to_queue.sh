a04fca1e38b5644222a0adbbb2e19965f0c3b1a0
Ubuntu 19.10 <taner.mustafa.98@gmail.com>
2020-05-10 15:06:57 -0700

    check

--- to_queue.sh
+++ to_queue.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-#qsub -q hp-sl.q -b y -wd ~/CUDA-Hashtable ./module_load.sh
-qsub -q ibm-dp.q -b y -wd ~/CUDA-Hashtable ./module_load.sh
+qsub -q hp-sl.q -b y -wd ~/CUDA-Hashtable ./module_load.sh
+#qsub -q ibm-dp.q -b y -wd ~/CUDA-Hashtable ./module_load.sh
 qstat
