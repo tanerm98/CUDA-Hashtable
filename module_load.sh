5ecf2b9cdfde3e951639867f54f60176bb335f4a
Ubuntu 19.10 <taner.mustafa.98@gmail.com>
2020-05-10 15:16:47 -0700

    test

--- module_load.sh
+++ module_load.sh
@@ -5,7 +5,6 @@ module load compilers/gnu-5.4.0
 
 echo "====================LOAD CUDA MODULE======================="
 module load libraries/cuda
-#module load libraries/cuda-8.0
 
 echo "======================MAKE CLEAN==========================="
 make clean
@@ -13,11 +12,11 @@ make clean
 echo "=========================MAKE=============================="
 make
 
+echo "========================DEBUG=============================="
+./gpu_hashtable 1000000 1
+
 echo "========================BENCH=============================="
 python3.5 bench.py
-
-echo "========================DEBUG=============================="
-./gpu_hashtable 1000 1
 
 echo "======================MAKE CLEAN==========================="
 make clean
